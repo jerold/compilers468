cf9c5373c7befeb80252322cff6504c190114aa3
clarkrabel <clarkrabel77@gmail.com>
2013-02-05 20:17:11 -0700

    my parser methods implemented

--- Compiler/src/Parser.java
+++ Compiler/src/Parser.java
@@ -10,6 +10,7 @@ public class Parser {
 	public int run(){
 		while (!scanner.endOfFile()) {
 			lookAhead = scanner.getToken();
+			lookAhead.describe();
 			if(lookAhead.getLexeme().equals("program")){
 				program();
 			}
@@ -105,63 +106,250 @@ public class Parser {
 	}
 	//Clark's section
 	private void valueParameterSection() {
-		
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_identifier":
+			identifierList();
+			match(":");
+			type();
+			break;	
+		default:
+			handleError();
+		}
 	}
 	private void variableParameterSection() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_var":
+			match("var");
+			variableParameterSection();
+			match(":");
+			type();
+			break;		
+		default:
+			handleError();
+		}
 		
 	}
 	private void compoundStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_begin":
+			match("begin");
+			statementSequence();
+			match("end");
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void statementSequence() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_begin":
+			statement();
+			break;
+		case "mp_for":
+			statement();
+			break;
+		case "mp_if":
+			statement();
+			break;
+		case "mp_read":
+			statement();
+			break;
+		case "mp_repeat":
+			statement();
+			break;
+		case "mp_while":
+			statement();
+			break;
+		case "mp_write":
+			statement();
+			break;
+		case "mp_identifier":
+			statement();
+			break;
+		case "mp_scolon":
+			match(";");
+			statement();
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void statement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_begin":
+			compoundStatement();
+			break;
+		case "mp_for":
+			compoundStatement();
+			break;
+		case "mp_if":
+			compoundStatement();
+			break;
+		case "mp_read":
+			simpleStatement();
+			break;
+		case "mp_repeat":
+			compoundStatement();
+			break;
+		case "mp_while":
+			compoundStatement();
+			break;
+		case "mp_write":
+			simpleStatement();
+			break;
+		case "mp_identifier":
+			simpleStatement();
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void simpleStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_read":
+			readStatement();
+			break;
+		case "mp_write":
+			writeStatement();
+			break;
+			//seems ambiguous here???
+		case "mp_identifier":
+			assignmentStatement();
+			procedureStatement();
+			break;
+		case "":
+			emptyStatement();
+		default:
+			handleError();
+		}
 		
 	}
 	private void structuredStatement() {
-		
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_begin":
+			compoundStatement();
+			break;
+		case "mp_for":
+			compoundStatement();
+			break;
+		case "mp_if":
+			conditionalStatement();
+			break;
+		case "mp_repeat":
+			compoundStatement();
+			break;
+		case "mp_while":
+			compoundStatement();
+			break;		
+		default:
+			handleError();
+		}
 	}
 	private void conditionalStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_if":
+			ifStatement();
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void repetitiveStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_while":
+			whileStatement();
+			break;
+		case "mp_repeat":
+			repeatStatement();
+			break;
+		case "mp_for":
+			forStatement();
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void emptyStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "":
+			match("");
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void readStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_read":
+			match("read");
+			readParameterList();
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void writeStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_write":
+			match("write");
+			writeParameterList();
+			break;
+		default:
+			handleError();
+		}
 		
 	}
+	//this seems ambiguous also??
 	private void assignmentStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_identifier":
+			//on the next two lines
+			variable();
+			functionIdentifier();
+			match(":=");
+			expression();
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void procedureStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_identifier":
+			procedureIdentifier();
+			break;
+		case "mp_lparen":
+			match("(");
+			actualParameterList();
+			match(")");
+			break;
+		default:
+			handleError();
+		}
 		
 	}
 	private void ifStatement() {
-		
+		switch(lookAhead.getIdentifier()){
+		case "mp_if":
+			match("if");
+			booleanExpression();
+			match("then");
+			statement();
+			break;
+		case "mp_else":
+			match("else");
+			statement();
+		default:
+			handleError();
+		}
 		
 	}
 	
@@ -286,7 +474,7 @@ public class Parser {
 		
 		
 	}
-	private void fuctionIdentifier() {
+	private void functionIdentifier() {
 		
 		
 	}
